201da0233c0476354fba450787d39e8802eb1ef2
HiTechCharles <hitechcharles@gmail.com>
2024-11-05 19:42:20 -0500

    Fully functional
    This version allows automatic printing of the menu when done.  The previous versions only saved it to disk.  Sections for monday- friday, and dates.

---
+++ TTMA/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace TTMA
+    {
+    partial class TTMAMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+            #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ReadBTN = new System.Windows.Forms.Button();
+            this.PrintBTN = new System.Windows.Forms.Button();
+            this.DatesLBL = new System.Windows.Forms.Label();
+            this.MondayLBL = new System.Windows.Forms.Label();
+            this.TuesdayLBL = new System.Windows.Forms.Label();
+            this.WednesdayLBL = new System.Windows.Forms.Label();
+            this.FLunchLBL = new System.Windows.Forms.Label();
+            this.FBreakfastLBL = new System.Windows.Forms.Label();
+            this.ThursdayLBL = new System.Windows.Forms.Label();
+            this.DatesTB = new System.Windows.Forms.TextBox();
+            this.MondayTB = new System.Windows.Forms.TextBox();
+            this.WednesdayTB = new System.Windows.Forms.TextBox();
+            this.TuesdayTB = new System.Windows.Forms.TextBox();
+            this.FBreakfastTB = new System.Windows.Forms.TextBox();
+            this.FLunchTB = new System.Windows.Forms.TextBox();
+            this.ThursdayTB = new System.Windows.Forms.TextBox();
+            this.HelpBTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ReadBTN
+            // 
+            this.ReadBTN.AccessibleName = "Read Entire Menu";
+            this.ReadBTN.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ReadBTN.Location = new System.Drawing.Point(511, 599);
+            this.ReadBTN.Name = "ReadBTN";
+            this.ReadBTN.Size = new System.Drawing.Size(175, 75);
+            this.ReadBTN.TabIndex = 15;
+            this.ReadBTN.Text = "R&ead";
+            this.ReadBTN.UseVisualStyleBackColor = true;
+            this.ReadBTN.Click += new System.EventHandler(this.ReadBTN_Click);
+            // 
+            // PrintBTN
+            // 
+            this.PrintBTN.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.PrintBTN.Location = new System.Drawing.Point(706, 599);
+            this.PrintBTN.Name = "PrintBTN";
+            this.PrintBTN.Size = new System.Drawing.Size(175, 75);
+            this.PrintBTN.TabIndex = 16;
+            this.PrintBTN.Text = "&Print";
+            this.PrintBTN.UseVisualStyleBackColor = true;
+            this.PrintBTN.Click += new System.EventHandler(this.PrintBTN_Click);
+            // 
+            // DatesLBL
+            // 
+            this.DatesLBL.AutoSize = true;
+            this.DatesLBL.Location = new System.Drawing.Point(178, 29);
+            this.DatesLBL.Name = "DatesLBL";
+            this.DatesLBL.Size = new System.Drawing.Size(108, 39);
+            this.DatesLBL.TabIndex = 0;
+            this.DatesLBL.Text = "&Dates";
+            // 
+            // MondayLBL
+            // 
+            this.MondayLBL.AutoSize = true;
+            this.MondayLBL.Location = new System.Drawing.Point(143, 113);
+            this.MondayLBL.Name = "MondayLBL";
+            this.MondayLBL.Size = new System.Drawing.Size(143, 39);
+            this.MondayLBL.TabIndex = 2;
+            this.MondayLBL.Text = "&Monday";
+            // 
+            // TuesdayLBL
+            // 
+            this.TuesdayLBL.AutoSize = true;
+            this.TuesdayLBL.Location = new System.Drawing.Point(137, 197);
+            this.TuesdayLBL.Name = "TuesdayLBL";
+            this.TuesdayLBL.Size = new System.Drawing.Size(149, 39);
+            this.TuesdayLBL.TabIndex = 4;
+            this.TuesdayLBL.Text = "&Tuesday";
+            // 
+            // WednesdayLBL
+            // 
+            this.WednesdayLBL.AutoSize = true;
+            this.WednesdayLBL.Location = new System.Drawing.Point(85, 281);
+            this.WednesdayLBL.Name = "WednesdayLBL";
+            this.WednesdayLBL.Size = new System.Drawing.Size(201, 39);
+            this.WednesdayLBL.TabIndex = 6;
+            this.WednesdayLBL.Text = "&Wednesday";
+            // 
+            // FLunchLBL
+            // 
+            this.FLunchLBL.AutoSize = true;
+            this.FLunchLBL.Location = new System.Drawing.Point(65, 533);
+            this.FLunchLBL.Name = "FLunchLBL";
+            this.FLunchLBL.Size = new System.Drawing.Size(221, 39);
+            this.FLunchLBL.TabIndex = 12;
+            this.FLunchLBL.Text = "Friday &Lunch";
+            // 
+            // FBreakfastLBL
+            // 
+            this.FBreakfastLBL.AutoSize = true;
+            this.FBreakfastLBL.Location = new System.Drawing.Point(-2, 445);
+            this.FBreakfastLBL.Name = "FBreakfastLBL";
+            this.FBreakfastLBL.Size = new System.Drawing.Size(288, 39);
+            this.FBreakfastLBL.TabIndex = 10;
+            this.FBreakfastLBL.Text = "Friday  &Breakfast";
+            // 
+            // ThursdayLBL
+            // 
+            this.ThursdayLBL.AutoSize = true;
+            this.ThursdayLBL.Location = new System.Drawing.Point(122, 361);
+            this.ThursdayLBL.Name = "ThursdayLBL";
+            this.ThursdayLBL.Size = new System.Drawing.Size(164, 39);
+            this.ThursdayLBL.TabIndex = 8;
+            this.ThursdayLBL.Text = "Thu&rsday";
+            // 
+            // DatesTB
+            // 
+            this.DatesTB.Location = new System.Drawing.Point(316, 22);
+            this.DatesTB.Name = "DatesTB";
+            this.DatesTB.Size = new System.Drawing.Size(565, 46);
+            this.DatesTB.TabIndex = 1;
+            // 
+            // MondayTB
+            // 
+            this.MondayTB.Location = new System.Drawing.Point(316, 106);
+            this.MondayTB.Name = "MondayTB";
+            this.MondayTB.Size = new System.Drawing.Size(565, 46);
+            this.MondayTB.TabIndex = 3;
+            // 
+            // WednesdayTB
+            // 
+            this.WednesdayTB.Location = new System.Drawing.Point(316, 274);
+            this.WednesdayTB.Name = "WednesdayTB";
+            this.WednesdayTB.Size = new System.Drawing.Size(565, 46);
+            this.WednesdayTB.TabIndex = 7;
+            // 
+            // TuesdayTB
+            // 
+            this.TuesdayTB.Location = new System.Drawing.Point(316, 190);
+            this.TuesdayTB.Name = "TuesdayTB";
+            this.TuesdayTB.Size = new System.Drawing.Size(565, 46);
+            this.TuesdayTB.TabIndex = 5;
+            // 
+            // FBreakfastTB
+            // 
+            this.FBreakfastTB.Location = new System.Drawing.Point(316, 442);
+            this.FBreakfastTB.Name = "FBreakfastTB";
+            this.FBreakfastTB.Size = new System.Drawing.Size(565, 46);
+            this.FBreakfastTB.TabIndex = 11;
+            // 
+            // FLunchTB
+            // 
+            this.FLunchTB.Location = new System.Drawing.Point(316, 526);
+            this.FLunchTB.Name = "FLunchTB";
+            this.FLunchTB.Size = new System.Drawing.Size(565, 46);
+            this.FLunchTB.TabIndex = 13;
+            // 
+            // ThursdayTB
+            // 
+            this.ThursdayTB.Location = new System.Drawing.Point(316, 358);
+            this.ThursdayTB.Name = "ThursdayTB";
+            this.ThursdayTB.Size = new System.Drawing.Size(565, 46);
+            this.ThursdayTB.TabIndex = 9;
+            // 
+            // HelpBTN
+            // 
+            this.HelpBTN.AccessibleName = "Help";
+            this.HelpBTN.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.HelpBTN.Location = new System.Drawing.Point(316, 599);
+            this.HelpBTN.Name = "HelpBTN";
+            this.HelpBTN.Size = new System.Drawing.Size(175, 75);
+            this.HelpBTN.TabIndex = 14;
+            this.HelpBTN.Text = "&Help";
+            this.HelpBTN.UseVisualStyleBackColor = true;
+            this.HelpBTN.Click += new System.EventHandler(this.HelpBTN_Click);
+            // 
+            // TTMAMain
+            // 
+            this.AccessibleDescription = "";
+            this.AccessibleName = "Totally Tiffany\'s Menu Assistant";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 39F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(899, 698);
+            this.Controls.Add(this.HelpBTN);
+            this.Controls.Add(this.ThursdayTB);
+            this.Controls.Add(this.FBreakfastTB);
+            this.Controls.Add(this.FLunchTB);
+            this.Controls.Add(this.WednesdayTB);
+            this.Controls.Add(this.TuesdayTB);
+            this.Controls.Add(this.MondayTB);
+            this.Controls.Add(this.DatesTB);
+            this.Controls.Add(this.FLunchLBL);
+            this.Controls.Add(this.FBreakfastLBL);
+            this.Controls.Add(this.ThursdayLBL);
+            this.Controls.Add(this.WednesdayLBL);
+            this.Controls.Add(this.TuesdayLBL);
+            this.Controls.Add(this.MondayLBL);
+            this.Controls.Add(this.DatesLBL);
+            this.Controls.Add(this.PrintBTN);
+            this.Controls.Add(this.ReadBTN);
+            this.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.MaximizeBox = false;
+            this.Name = "TTMAMain";
+            this.Text = "Totally Tiffany\'s Menu Assistant";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ReadBTN;
+        private System.Windows.Forms.Button PrintBTN;
+        private System.Windows.Forms.Label DatesLBL;
+        private System.Windows.Forms.Label MondayLBL;
+        private System.Windows.Forms.Label TuesdayLBL;
+        private System.Windows.Forms.Label WednesdayLBL;
+        private System.Windows.Forms.Label FLunchLBL;
+        private System.Windows.Forms.Label FBreakfastLBL;
+        private System.Windows.Forms.Label ThursdayLBL;
+        private System.Windows.Forms.TextBox DatesTB;
+        private System.Windows.Forms.TextBox MondayTB;
+        private System.Windows.Forms.TextBox WednesdayTB;
+        private System.Windows.Forms.TextBox TuesdayTB;
+        private System.Windows.Forms.TextBox FBreakfastTB;
+        private System.Windows.Forms.TextBox FLunchTB;
+        private System.Windows.Forms.TextBox ThursdayTB;
+        private System.Windows.Forms.Button HelpBTN;
+    }
+}
+
---
+++ TTMA/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Windows.Forms;  //windows forms
+using System.Speech.Synthesis;  //for speech synthesis
+
+namespace TTMA
+{
+    public partial class TTMAMain : Form
+    {
+        public static string[] Menu = new string[8];  //stores lines from 7 textboxes
+
+        public TTMAMain()
+        {
+            InitializeComponent();  
+        }
+
+        bool GatherText()  //put contents of textboxes in Menu array
+        {
+            Menu[0] = "Totally Tiffany's Cafe";  //static business name
+            Menu[1] = DatesTB.Text; 
+            Menu[2] = "Monday - " + MondayTB.Text;
+            Menu[3] = "Tuesday - " + TuesdayTB.Text;
+            Menu[4] = "Wednesday - " + WednesdayTB.Text;
+            Menu[5] = "Thursday - " + ThursdayTB.Text;
+            Menu[6] = "Friday Breakfast - " + FBreakfastTB.Text;
+            Menu[7] = "Friday Lunch - " + FLunchTB.Text;
+
+            //add up total length of all textboxes
+            int TotalTextLength = DatesTB.Text.Length;
+            TotalTextLength += MondayTB.Text.Length;
+            TotalTextLength += TuesdayTB.Text.Length;
+            TotalTextLength += WednesdayTB.Text.Length;
+            TotalTextLength += ThursdayTB.Text.Length;
+            TotalTextLength += FBreakfastTB.Text.Length;
+            TotalTextLength += FLunchTB.Text.Length;
+
+            if (TotalTextLength > 0)  //if all boxes not empty 
+                return true;  //return true
+            else
+                return false; //return false
+
+        }
+
+        private void ReadBTN_Click(object sender, EventArgs e)  //Read button speaks entire menu
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();  //adds speech ability to this function
+            //if no text in textboxes, don't speak empty strings
+            if ( GatherText() == false )   //reset menu array with current textbox contents
+            {
+                synth.Speak("All menu sections are empty, nothing to speak.");  //alert user
+                return;  //exit function
+            }
+
+            for (int i = 0; i < Menu.Length; i++)  //loop through elements
+            {
+                synth.Speak(Menu[i]);  //speak contents of element
+            }
+        }
+
+        private void HelpBTN_Click(object sender, EventArgs e)  //reads help text instead of dialog box
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();  //make the computer chatty
+
+            synth.Speak("Use the following keyboard keys for Totally Tiffany's Menu Assistant:");
+            synth.Speak("Type in what you are serving for each day of the week.");
+            synth.Speak("Up Arrow - Reads the current line.");
+            synth.Speak("TAB - switches between sections.");
+            synth.Speak("Read Button - Speaks the entire menu out loud.");
+            synth.Speak("Print Button - Saves everything to a file, then prints it.)");
+        }
+
+        private void PrintBTN_Click(object sender, EventArgs e)
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();  //add speech capability
+
+            if (GatherText() == false)   //reset menu array with current textbox contents
+            {
+                synth.Speak("All menu sections are empty, nothing to print.");  //alert user
+                return;  //exit function
+            }
+
+            //speak text saving menu then printing file
+            synth.Speak("Saving the menu, and printing 4 copies.");
+
+            //open a file in OneDrive\Documents\ttma.txt
+            System.IO.StreamWriter MenuFile = new System.IO.StreamWriter(Environment.GetEnvironmentVariable("onedriveconsumer") + "\\documents\\TTMA.txt");
+            GatherText();  //update menu array
+            MenuFile.WriteLine(Menu[0]);  //business name
+
+            for (int i = 1; i < Menu.Length; i++)  //for each element, save to file with blank line after
+            {
+                MenuFile.WriteLine(Menu[i]);
+                MenuFile.WriteLine();
+            }
+
+            MenuFile.Close();  //close file
+                               //print 4 copies of the file that was jest made
+
+            for (int p = 0; p < 4; p++)
+            {
+                System.Diagnostics.Process.Start("notepad", "/p " + Environment.GetEnvironmentVariable("onedriveconsumer") + "\\documents\\TTMA.txt");
+            }
+        }
+    }  //end class
+}  //end namespace
